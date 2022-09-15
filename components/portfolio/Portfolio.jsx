565624649ec48e979706491c3a77ffedbfe940cb
karim <karimelsisi27@gmail.com>
2022-09-15 16:46:51 +0200

    Improve SEO

--- components/portfolio/Portfolio.jsx
+++ components/portfolio/Portfolio.jsx
@@ -15,35 +15,35 @@ export default function Portfolio() {
     <section className='bg-white min-h-fit w-full flex flex-col lg:flex-row lg:flex-wrap justify-around' id='portfolio'>
             
             <div className='w-3/4 lg:w-2/12 mx-10 my-10 hover:scale-110 transition duration-500'>
-                <Image src={catchyAd} alt="catchyAd"  className='rounded-2xl'/>
+                <Image src={"https://images.unsplash.com/photo-1602333869619-f05b7f19d3c9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fGFkdmVydGlzaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"} alt="catchyAd"  className='rounded-2xl'  width={400} height={600} />
             </div>
                         
             <div className='w-3/4 lg:w-2/12 mx-10 my-10 hover:scale-110 transition duration-500'>
-                <Image src={cosmeticsAd} alt="cosmeticsAd"  className='rounded-2xl'/>
+                <Image src={"https://images.unsplash.com/photo-1546333603-2895edefc462?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTMxfHxhZHZlcnRpc2luZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"} alt="cosmeticsAd"  className='rounded-2xl' width={400} height={600} />
             </div>
                         
             <div className='w-3/4 lg:w-2/12 mx-10 my-10 hover:scale-110 transition duration-500'>
-                <Image src={cyberCityAd} alt="cyberCityAd"  className='rounded-2xl'/>
+                <Image src={"https://images.unsplash.com/photo-1541537103745-ea3429c65dc4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YWR2ZXJ0aXNpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"} alt="cyberCityAd"  className='rounded-2xl' width={400} height={600} />
             </div>
                         
             <div className='w-3/4 lg:w-2/12 mx-10 my-10 hover:scale-110 transition duration-500'>
-                <Image src={fruitsAd} alt="fruitsAd"  className='rounded-2xl'/>
+                <Image src={fruitsAd} alt="fruitsAd"  className='rounded-2xl' width={400} height={600} />
             </div>
             
             <div className='w-3/4 lg:w-2/12 mx-10 my-10 hover:scale-110 transition duration-500'>
-                <Image src={juiceAd} alt="juiceAd"  className='rounded-2xl'/>
+                <Image src={"https://images.unsplash.com/photo-1599360889420-da1afaba9edc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjR8fGFkdmVydGlzaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"} alt="juiceAd"  className='rounded-2xl' width={400} height={600} />
             </div>
             
             <div className='w-3/4 lg:w-2/12 mx-10 my-10 hover:scale-110 transition duration-500'>
-                <Image src={porcheAd} alt="porcheAd"  className='rounded-2xl'/>
+                <Image src={"https://images.unsplash.com/photo-1583717754414-05b42568e7ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTY0fHxhZHZlcnRpc2luZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"} alt="porcheAd"  className='rounded-2xl' width={400} height={600} />
             </div>
             
             <div className='w-3/4 lg:w-2/12 mx-10 my-10 hover:scale-110 transition duration-500'>
-                <Image src={studyBlueAd} alt="studyBlueAd"  className='rounded-2xl'/>
+                <Image src={"https://images.unsplash.com/photo-1617695744007-68ef55752789?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGFkdmVydGlzaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"} alt="studyBlueAd"  className='rounded-2xl' width={400} height={600} />
             </div>
             
             <div className='w-3/4 lg:w-2/12 mx-10 my-10 hover:scale-110 transition duration-500'>
-                <Image src={studyOrangeAd} alt="studyOrangeAd"  className='rounded-2xl'/>
+                <Image src={"https://images.unsplash.com/photo-1558365916-848463c5d803?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGFkdmVydGlzaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"} alt="studyOrangeAd"  className='rounded-2xl' width={400} height={600} />
             </div>
              
             
--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en'
+  },
+  images: {
+    domains: ["images.pexels.com", "images.unsplash.com"]
+  }
 }
 
 module.exports = nextConfig
